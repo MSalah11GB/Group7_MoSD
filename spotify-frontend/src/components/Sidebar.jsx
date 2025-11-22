02a77ebeb3095eb18c3510eac5a4605608a352a3
unknown <anh.pq220071@sis.hust.edu.vn>
2025-11-22 10:45:02 +0700

    feat: fe context

--- spotify-frontend/src/components/Sidebar.jsx
+++ spotify-frontend/src/components/Sidebar.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from 'react-router-dom';
 import { assets } from '../assets/assets';
 
 const Sidebar = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
       <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
