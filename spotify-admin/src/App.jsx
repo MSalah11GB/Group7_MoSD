688c792668affc3d91a462fab27f029e5546afe5
Dinh Ngoc Lap Thanh <thanhdinhngoclap123@gmail.com>
2025-12-20 22:19:36 +0700

    add addsong components, fix importing sidebar

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -13,14 +13,16 @@ import ListAlbum from './pages/ListAlbum';
 import ListArtist from './pages/ListArtist';
 import ListGenre from './pages/ListGenre';
 import EditSong from './pages/EditSong';
-import SideBar from './components/SideBar';
+import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
+
+export const url = 'http://localhost:4000'
 
 const App = () => {
   return (
     <div className='flex items-start min-h-screen'>
       <ToastContainer/>
-      <SideBar/>
+      <Sidebar/>
       <div className='flex-1 h-screen overflow-y-auto bg-[#F3FFF7]'>
         <Navbar/>
         <div className='pt-8 pl-5 sm:pt-12 sm:pl-12'>
