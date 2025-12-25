762b05f7a7280d1cd673909163784e8de8219c86
Trinh Manh Quynh <otime33333@gmail.com>
2025-12-26 00:29:29 +0700

    Update MongoDB connection URI for Musicify

--- spotify-backend/src/config/mongodb.js
+++ spotify-backend/src/config/mongodb.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         console.log("connection establised");
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URI}`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/Musicify`);
 }
 
 export default connectDB;
